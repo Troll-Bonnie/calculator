86e305bb4c03a648c5cb077b19d334eba08a8de3
unknown <Sahar1student@gmail.com>
2022-11-29 17:52:22 +0100

    class Menu Manager - done

--- MenuManager.cs
+++ MenuManager.cs
@@ -1,4 +1,18 @@
 class MenuManager
 {
-    
+    public void Display() 
+    {
+        Console.WriteLine("================");
+        Console.WriteLine("Arifm calculator");
+        Console.WriteLine("================");
+        Console.WriteLine(" 1 - Adding");
+        Console.WriteLine(" 2 - Substracting");
+        Console.WriteLine(" 3 - Multiplying");
+        Console.WriteLine(" 4 - Dividing");
+        Console.WriteLine("================");
+    }
+    public int GetChoice() 
+    {
+        
+    }
 }
