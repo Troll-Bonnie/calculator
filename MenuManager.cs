6436e2fe61597aa391c8caa96969bdadf8db33a5
unknown <Sahar1student@gmail.com>
2022-12-01 13:09:47 +0100

    It is working now

--- MenuManager.cs
+++ MenuManager.cs
@@ -9,10 +9,19 @@ class MenuManager
         Console.WriteLine(" 2 - Substracting");
         Console.WriteLine(" 3 - Multiplying");
         Console.WriteLine(" 4 - Dividing");
+        Console.WriteLine(" 5 - Exit");
         Console.WriteLine("================");
     }
     public int GetChoice() 
     {
-        
+        Console.WriteLine("Enter operation num");
+        return int.Parse(Console.ReadLine());
+    }
+
+    public bool AllowContinue()
+    {
+        Console.WriteLine("Continue (y/n)? - ");
+        string allow = Console.ReadLine();
+        return (allow == "y");
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Main Module");
+MenuManager menu = new MenuManager();
+DialogManager dialog = new DialogManager();
+int choice = 0;
+do
+{
+    Console.Clear();
+    menu.Display();
+    menu.GetChoice();
+    switch (choice)
+    {
+        case 1:
+            dialog.AddDialog();
+            break;
+        case 1:
+            dialog.SubDialog();
+            break;
+        case 1:
+            dialog.MultDialog();
+            break;
+        case 1:
+            dialog.DivDialog();
+            break;
+        default:
+            break;
+    }
 
+}while(menu.AllowContinue() || choice == 5);
